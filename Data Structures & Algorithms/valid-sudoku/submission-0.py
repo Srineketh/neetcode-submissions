5b133dffe01e45779b31d04935fd09aee611457d
Srineketh <74057295+Srineketh@users.noreply.github.com>
2026-07-02 20:00:45 +0100

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = {}
+        cols = {}
+        squares = {}
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == '.':
+                    continue
+                if (rows.get(row) and board[row][col] in rows.get(row) or
+                    cols.get(col) and board[row][col] in cols.get(col) or
+                    squares.get((row // 3) * 3 + (col // 3)) and board[row][col] in squares.get((row // 3) * 3 + (col // 3))):
+                    return False
+                if not rows.get(row):
+                    rows[row] = [board[row][col]]
+                else:
+                    rows[row].append(board[row][col])
+                if not cols.get(col):
+                    cols[col] = [board[row][col]]
+                else:
+                    cols[col].append(board[row][col])
+                if not squares.get((row // 3) * 3 + (col // 3)):
+                    squares[(row // 3) * 3 + (col // 3)] = [board[row][col]]
+                else:
+                    squares[(row // 3) * 3 + (col // 3)].append(board[row][col])
+        return True
+            
