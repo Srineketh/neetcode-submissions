6e965ec2f79386c94f483a730f8c8d34494726ea
Srineketh <74057295+Srineketh@users.noreply.github.com>
2026-09-24 20:13:36 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def alphanum(self,s):
+        return (ord('A') <= ord(s) <= ord('Z') or
+                ord('a') <= ord(s) <= ord('z') or
+                ord('0') <= ord(s) <= ord('9'))
+
+    def isPalindrome(self, s: str) -> bool:
+        processed = ""
+        for i in s:
+            if self.alphanum(i):
+                processed += i
+        processed = processed.lower()
+        i = 0
+        j = len(processed) // 2 if len(processed) % 2 == 0 else (len(processed) // 2 ) + 1
+        for i in range(j):
+            if processed[i] != processed[-(i+1)]:
+                return False
+        return True
